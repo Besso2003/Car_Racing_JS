83b4754a23363116474ff5fc0a166d6b0624de2d
Heba Maher <hebamaher25@gmail.com>
2026-01-19 01:24:23 +0200

    implemented and tested Obstacle creation

---
+++ js/core/constants.js
@@ -0,0 +1,7 @@
+export const GAME_HEIGHT = 600;
+
+export const LANES = [
+  100, // left
+  200, // middle
+  300  // right
+];
--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ import UIManager from './ui/UIManager.js';
 import ScoreManager from './systems/ScoreManager.js';
 import StorageManager from './storage/StorageManager.js';
 
+import Obstacle from "./entities/Obstacle.js";
+import { LANES, GAME_HEIGHT } from "./core/constants.js";
+
 // Initialize managers
 const uiManager = new UIManager();
 const scoreManager = new ScoreManager();
@@ -40,3 +43,17 @@ function gameLoop() {
     }
 }
 
+// Test obstacle creation and movement
+let lastTime = performance.now();
+const obstacle = new Obstacle(1, LANES, 300);
+
+function testLoop(time) {
+  const deltaTime = (time - lastTime) / 1000;
+  lastTime = time;
+
+  obstacle.update(deltaTime);
+
+  requestAnimationFrame(testLoop);
+}
+
+requestAnimationFrame(testLoop);
